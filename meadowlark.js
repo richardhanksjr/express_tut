c7edb5c4fea4306e3b5c10250754124b9186f8c8
Richard Hanks <richardhanksjr@gmail.com>
2018-01-05 09:03:21 -0500

    Added initial handlebar pages and layout

--- meadowlark.js
+++ meadowlark.js
@@ -4,31 +4,32 @@ var app = express();
 
 app.set("port", process.env.PORT || 3000);
 
+var handlebars = require("express-handlebars").create({defaultLayout: "main"});
+app.engine("handlebars", handlebars.engine);
+app.set("view engine", "handlebars");
+app.use(express.static(__dirname + "/public"));
+
 
 app.get("/", function(req, res){
-  res.type("text/plain");
-  res.send("Meadowlark Travel");
+  res.render("home");
 });
 app.get("/about", function(req, res){
-  res.type("text/plain");
-  res.send("About meadowlark Travel");
+  res.render("about");
 });
 
 
 // custom 404 page
 app.use(function(req, res){
-  res.type('text/plain');
   res.status(404);
-  res.send('404 - Not Found');
+  res.render('404');
 });
 
 // Custome 500 page
 
 app.use(function(err, req, res, next){
   console.log(err.stack);
-  res.type("text/plain");
   res.status(500);
-  res.send("500-Server Error");
+  res.render('500');
 });
 
 app.listen(app.get("port"), function(){
